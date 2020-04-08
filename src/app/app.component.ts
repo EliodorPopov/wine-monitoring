7b6ac4a85bf097db7f89fd8dc96d6add3b12c5fa
Eliodor Popov <eliodor.popov@outlook.com>
2020-04-08 13:41:54 +0300

    add appinsights

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApplicationInsights, PageViewPerformance } from '@microsoft/applicationinsights-web';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'wine-monitoring';
+
+  ngOnInit() {
+    const appInsights = new ApplicationInsights({
+      config: {
+        instrumentationKey: '696b6441-c6e4-49cb-9c1d-a6894bdf211a',
+      },
+    });
+    appInsights.loadAppInsights();
+    appInsights.trackPageView();
+  }
 }
